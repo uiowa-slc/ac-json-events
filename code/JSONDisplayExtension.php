b266a525fd2bba98247229bfa0ceb01fe6032624
Dustin Quam <quamsta@gmail.com>
2014-05-06 11:36:35 -0500

    lil bit of refactoring

--- code/JSONDisplayExtension.php
+++ code/JSONDisplayExtension.php
@@ -1,75 +1,96 @@
 <?php
 class JSONDisplayExtension extends DataExtension{
 
-	public function AfterClassFeed($feedURL="http://afterclass.uiowa.edu/events/feed/json") {
-		
-		$outfeed = new ArrayList();
-		$JSON_string = file_get_contents($feedURL);
-		$feed_array = json_decode($JSON_string, TRUE);
-		 
-		foreach($feed_array['events'] as $item) {
-					 	
-		 	$id = new Text('ID');
-		 	$id = $item['id'];
-		 	
-		 	$name = new Text('name');
-		 	$name->setValue($item['name']);
+	private function parseEvent($rawEvent){
 
-		 	$link = new Text('Link');
-		 	$link->setValue($item['link']);
-		 	
-		 	$more_info_link = new Text('more_info_link');
-		 	$more_info_link->setValue($item['more_info_link']);
-		 	
-		 	$imageURL = new Text('ImageURL');
-			$imageURL->setValue($item['image']);	
-	
-			$cancel_note = new Text('cancel_note');
-			$cancel_note->setValue($item['cancel_note']);
-		 	
-			$nextDateTime = new SS_Datetime('NextDateTime');
-			$nextDateTime->setValue(strtotime($item['dates'][0]['start_date'].' '.$item['dates'][0]['start_time']));
+	 	$id = new Text('ID');
+	 	$id = $rawEvent['id'];
+	 	
+	 	$name = new Text('name');
+	 	$name->setValue($rawEvent['name']);
+
+	 	$link = new Text('Link');
+	 	$link->setValue($rawEvent['link']);
+	 	
+	 	$more_info_link = new Text('more_info_link');
+	 	$more_info_link->setValue($rawEvent['more_info_link']);
+	 	
+	 	$imageURL = new Text('ImageURL');
+		$imageURL->setValue($rawEvent['image']);	
+
+		$cancel_note = new Text('cancel_note');
+		$cancel_note->setValue($rawEvent['cancel_note']);
+	 	
+		$nextDateTime = new SS_Datetime('NextDateTime');
+		$nextDateTime->setValue(strtotime($rawEvent['dates'][0]['start_date'].' '.$rawEvent['dates'][0]['start_time']));
+		
+		$dateTimeCount = new Int('DateTimeCount');
+		$dateTimeCount->setValue(count($rawEvent['dates']));
+
+		$cost = new Text('Cost');
+		$cost->setValue($rawEvent['price']);
 			
-			$dateTimeCount = new Int('DateTimeCount');
-			$dateTimeCount->setValue(count($item['dates']));
+		$location = new Text('Location');
+		$location->setValue($rawEvent['location']);
 
-			$cost = new Text('Cost');
-			$cost->setValue($item['price']);
-				
-			$location = new Text('Location');
-			$location->setValue($item['location']);
-	
-			$venue = new Text('Venue');
-			if($item['venues']) {
-				$venue->setValue($item['venues'][0]['name']);
-			}
+		$venue = new Text('Venue');
+		if($rawEvent['venues']) {
+			$venue->setValue($rawEvent['venues'][0]['name']);
+		}
 
-			$sponsors = new Text('Sponsors');
-			if($item['sponsors']) {
-				$sponsors->setValue($item['sponsors'][0]['name']);
-			}
+		$sponsors = new Text('Sponsors');
+		if($rawEvent['sponsors']) {
+			$sponsors->setValue($rawEvent['sponsors'][0]['name']);
+		}
 
-			$event_types = new Text('Event Types');
-			if($item['event_types']) {
-				$event_types->setValue($item['event_types'][0]['name']);					
+		$event_types = new Text('Event Types');
+		if($rawEvent['event_types']) {
+			$event_types->setValue($rawEvent['event_types'][0]['name']);					
+		}
+
+		$parsedEvent = new ArrayData(array(
+		'ID'			=> $id,
+	    'Title'         => $name,
+	    'Link' => $link,
+	    'MoreInfoLink' => $more_info_link,
+	    'ImageURL' => $imageURL,
+	    'CancelNote' => $cancel_note,
+	    'NextDateTime'		=> $nextDateTime,
+	    'DateTimeCount' => $dateTimeCount,
+	    'Cost'		=> $cost,
+	    'Location'	=> $location,
+	    'Venue' => $venue,
+	    'Sponsors' => $sponsors,
+	    'EventTypes' => $event_types
+	    ));
+		return $parsedEvent;
+	}
+
+	public function AfterClassEvent($id){
+
+		$feedURL = 'http://afterclass.uiowa.edu/events/feed/';
+		$feed = new ArrayList();
+		$rawFeed = file_get_contents($feedURL);
+
+		$eventsDecoded = json_decode($rawFeed, TRUE);
+		 
+		foreach($eventsDecoded['events'] as $event) {
+			if($event['id'] == $id){
+				return $this->parseEvent($event);
 			}
+		}
+		return false;
+	}
+
+	public function AfterClassEvents($feedURL="http://afterclass.uiowa.edu/events/feed/json") {
 		
-			$outfeed->push(new ArrayData(array(
-			'ID'			=> $id,
-		    'Title'         => $name,
-		    'Link' => $link,
-		    'MoreInfoLink' => $more_info_link,
-		    'ImageURL' => $imageURL,
-		    'CancelNote' => $cancel_note,
-		    'NextDateTime'		=> $nextDateTime,
-		    'DateTimeCount' => $dateTimeCount,
-		    'Cost'		=> $cost,
-		    'Location'	=> $location,
-		    'Venue' => $venue,
-		    'Sponsors' => $sponsors,
-		    'EventTypes' => $event_types
-		    )));
+		$eventsList = new ArrayList();
+		$rawFeed = file_get_contents($feedURL);
+		$eventsDecoded = json_decode($rawFeed, TRUE);
+		 
+		foreach($eventsDecoded['events'] as $event) {
+			$eventsList->push($this->parseEvent($event));
 		}		
-		return $outfeed;   
+		return $eventsList;   
 	}
 }
