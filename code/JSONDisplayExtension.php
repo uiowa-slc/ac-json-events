c686e88b31a7bd15b6c7727a9ff8ed5a4adb014b
athaax <jonathansolis@dhcp80ff3112.dynamic.uiowa.edu>
2014-05-21 16:27:32 -0500

    Locallist forays
    Next Upcoming date dependent on the feed being chronological.

--- code/JSONDisplayExtension.php
+++ code/JSONDisplayExtension.php
@@ -4,10 +4,10 @@ class JSONDisplayExtension extends DataExtension{
 	private function parseEvent($rawEvent){
 
 	 	$id = new Text('ID');
-	 	$id = $rawEvent['id'];
+	 	$id->setValue($rawEvent['id']);
 	 	
-	 	$name = new Text('name');
-	 	$name->setValue($rawEvent['name']);
+	 	$title = new Text('Title');
+	 	$title->setValue($rawEvent['title']);
 
 	 	$link = new Text('Link');
 	 	$link->setValue($rawEvent['link']);
@@ -19,13 +19,24 @@ class JSONDisplayExtension extends DataExtension{
 	 	$facebook_event_link->setValue($rawEvent['facebook_event_link']);
 
 	 	$imageURL = new Text('ImageURL');
-		$imageURL->setValue($rawEvent['image']);	
+		$imageURL->setValue($rawEvent['photo_url']);	
 
 		$cancel_note = new Text('cancel_note');
 		$cancel_note->setValue($rawEvent['cancel_note']);
 	 	
 		$nextDateTime = new SS_Datetime('NextDateTime');
-		$nextDateTime->setValue(strtotime($rawEvent['dates'][0]['start_date'].' '.$rawEvent['dates'][0]['start_time']));
+		//$nextDateTime->setValue(strtotime($rawEvent['dates'][0]['start_date'].' '.$rawEvent['dates'][0]['start_time']));
+		$time = time();
+		
+		foreach($rawEvent['event_instances'] as $eventInstance) {
+			//print_r (strtotime(substr($eventInstance['event_instance']['start'], 0, 10)));
+			if (strtotime(substr($eventInstance['event_instance']['start'], 0, 10)) >= $time) {
+				$nextDateTime->setValue($eventInstance['event_instance']['start']);
+				break;
+			} else {
+				print_r ('nothing found');
+			}		
+		};
 		
 		$dateTimeCount = new Int('DateTimeCount');
 		$dateTimeCount->setValue(count($rawEvent['dates']));
@@ -54,51 +65,54 @@ class JSONDisplayExtension extends DataExtension{
 		}
 
 		$parsedEvent = new ArrayData(array(
-			'ID'			=> $id,
-		    'Title'         => $name,
-		    'Link' => $link,
+			'ID'				=> $id,
+		    'Title'         	=> $title,
+		    'Link' 				=> $link,
 		    'FacebookEventLink' => $facebook_event_link,
-		    'MoreInfoLink' => $more_info_link,
-		    'ImageURL' => $imageURL,
-		    'CancelNote' => $cancel_note,
+		    'MoreInfoLink' 		=> $more_info_link,
+		    'ImageURL'			=> $imageURL,
+		    'CancelNote' 		=> $cancel_note,
 		    'NextDateTime'		=> $nextDateTime,
-		    'DateTimeCount' => $dateTimeCount,
-		    'Cost'		=> $cost,
-		    'Location'	=> $location,
-		    'Venue' => $venue,
-		    'Sponsors' => $sponsors,
-		    'EventTypes' => $eventTypes
+		    'DateTimeCount'		=> $dateTimeCount,
+		    'Cost'				=> $cost,
+		    'Location'			=> $location,
+		    'Venue' 			=> $venue,
+		    'Sponsors' 			=> $sponsors,
+		    'EventTypes' 		=> $eventTypes
 	    ));
 		return $parsedEvent;
 	}
-
+		
+	public function AfterClassEvents($feedURL="http://hulk.imu.uiowa.edu/localist-api-examples/events.json") {
+		
+		$eventsList = new ArrayList();
+		$rawFeed = file_get_contents($feedURL);
+		$eventsDecoded = json_decode($rawFeed, TRUE);
+		$eventsArray = $eventsDecoded['events'];
+		foreach($eventsArray as $event) {
+			$eventsList->push($this->parseEvent($event['event']));
+		}		
+		return $eventsList;   
+	}
+	
 	public function AfterClassEvent($id){
 
-		$feedURL = 'http://afterclass.uiowa.edu/events/feed/';
+		$feedURL = 'http://hulk.imu.uiowa.edu/localist-api-examples/events.json';
 		$feed = new ArrayList();
 		$rawFeed = file_get_contents($feedURL);
-
 		$eventsDecoded = json_decode($rawFeed, TRUE);
-		
-		if(isset($eventsDecoded)){
-			foreach($eventsDecoded['events'] as $event) {
-				if($event['id'] == $id){
+		$eventsList = $eventsDecoded['events'];
+		if(isset($eventsList)){
+			//echo('hello, world');
+			//print_r ($eventsList);
+			foreach($eventsList as $event) {
+				//print_r ($event);
+				if($event['event']['id'] == $id){
 					return $this->parseEvent($event);
 				}
 			}
 		}
 		return false;
 	}
-
-	public function AfterClassEvents($feedURL="http://afterclass.uiowa.edu/events/feed/json") {
-		
-		$eventsList = new ArrayList();
-		$rawFeed = file_get_contents($feedURL);
-		$eventsDecoded = json_decode($rawFeed, TRUE);
-		 
-		foreach($eventsDecoded['events'] as $event) {
-			$eventsList->push($this->parseEvent($event));
-		}		
-		return $eventsList;   
-	}
+	
 }
