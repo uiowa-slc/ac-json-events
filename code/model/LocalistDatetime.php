71f5b94f1eaf4287e33baf36378ffdd9051d6a12
Dustin Quam <quamsta@gmail.com>
2014-07-02 12:34:46 -0500

    added some comments in LocalistDatetime

--- code/model/LocalistDatetime.php
+++ code/model/LocalistDatetime.php
@@ -2,6 +2,10 @@
 
 class LocalistDatetime extends SS_Datetime {
 
+
+	/* Link function might change to an internal link eventually, currently links to the
+	localist caliendar filter */
+
 	public function Link(){
 		$datestring = date("Y/n/j", strtotime($this->value));
 		$urlparts = array(
