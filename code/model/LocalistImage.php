452f4273d96875aae7c46717644b470bee156a16
Dustin Quam <dustinquam@me.com>
2015-01-23 10:53:54 -0600

    added absoluteURL getter to localist image

--- code/model/LocalistImage.php
+++ code/model/LocalistImage.php
@@ -5,41 +5,43 @@ class LocalistImage extends DataObject {
 	private static $db = array(
 		"Caption" => "Text",
 		"URL" => "Text",
-		"Credit" => "Text"
-
+		"Credit" => "Text",
 
 	);
 
-	public function getByID($id){
+	public function getByID($id) {
 
-		if(!isset($id) || $id == 0) return false;
-			$cache = new SimpleCache();
-
-			$feedParams = 'photos/'.$id;
-			$feedURL = LOCALIST_FEED_URL.$feedParams;
-
-			//print_r($feedURL);
-
-			$rawFeed = $cache->get_data( $feedURL, $feedURL );
-			$imagesDecoded = json_decode( $rawFeed, TRUE );
-
-			$image = $imagesDecoded['photo'];
-			if ( isset( $image ) ) {
-				$localistImage = new LocalistImage();
-				return $localistImage->parse( $image );
-			}
+		if (!isset($id) || $id == 0) {
 			return false;
 		}
+
+		$cache = new SimpleCache();
+
+		$feedParams = 'photos/' . $id;
+		$feedURL = LOCALIST_FEED_URL . $feedParams;
+
+		//print_r($feedURL);
+
+		$rawFeed = $cache->get_data($feedURL, $feedURL);
+		$imagesDecoded = json_decode($rawFeed, TRUE);
+
+		$image = $imagesDecoded['photo'];
+		if (isset($image)) {
+			$localistImage = new LocalistImage();
+			return $localistImage->parse($image);
+		}
+		return false;
+	}
 
 	/* Link function might change to an internal link eventually, currently links to the
 	localist caliendar filter */
 
 	/**
 	 * Convert an event in an array format (from Localist JSON Feed) to a LocalistEvent
-	 * @param array $rawEvent 
+	 * @param array $rawEvent
 	 * @return LocalistEvent
 	 */
-	public function parse($rawImage){
+	public function parse($rawImage) {
 		$this->ID = $rawImage['id'];
 		$this->Caption = $rawImage['caption'];
 		$this->URL = $rawImage['photo_url'];
@@ -47,12 +49,16 @@ class LocalistImage extends DataObject {
 		return $this;
 	}
 
-	public function getURL(){
-		if(isset($this->URL)){
+	public function getAbsoluteURL() {
+		return $this->getURL();
+	}
+
+	public function getURL() {
+		if (isset($this->URL)) {
 			return $this->getField('URL');
-		}else{
-			$themeFolder = 'themes/' . SSViewer::current_theme(); 
-			return $themeFolder.'/images/LocalistEventPlaceholder.jpg';
+		} else {
+			$themeFolder = 'themes/' . SSViewer::current_theme();
+			return $themeFolder . '/images/LocalistEventPlaceholder.jpg';
 		}
 	}
 
