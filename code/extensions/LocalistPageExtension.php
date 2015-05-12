a431403eb8fae7df711615212f0bea965136a0a8
Dustin Quam <dustinquam@me.com>
2015-05-12 13:44:15 -0500

    some fixes for picking out featured events

--- code/extensions/LocalistPageExtension.php
+++ code/extensions/LocalistPageExtension.php
@@ -1,9 +1,25 @@
 <?php
 
-class LocalistPageExtension extends DataExtension{
-	
+class LocalistPageExtension extends DataExtension {
+
 	public function LocalistCalendar() {
 		return LocalistCalendar::get()->First();
 	}
 
+	public function getJson($feedURL) {
+		$cache = new SimpleCache();
+		if ($rawFeed = $cache->get_data($feedURL, $feedURL)) {
+			$eventsDecoded = json_decode($rawFeed, TRUE);
+		} else {
+			$rawFeed = $cache->do_curl($feedURL);
+			$cache->set_cache($feedURL, $rawFeed);
+			$eventsDecoded = json_decode($rawFeed, TRUE);
+		}
+
+		if (!empty($eventsDecoded)) {
+			return $eventsDecoded;
+		} else {
+			return false;
+		}
+	}
 }
