143fcceefbf7fd25eb3011b79f488c359b188fbd
Dustin Quam <quamsta@gmail.com>
2020-05-12 15:22:43 -0500

    updates

--- code/extensions/UiCalendarPageExtension.php
+++ code/extensions/UiCalendarPageExtension.php
@@ -1,27 +1,42 @@
 <?php
 
 use SilverStripe\ORM\DataExtension;
+use quamsta\ApiCacher\FeedHelper;
 
 class UiCalendarPageExtension extends DataExtension {
 
 	public function UiCalendar() {
 		return UiCalendar::getOrCreate();
+
 	}
 
 	public function getJson($feedURL) {
-		$cache = new SimpleCache();
-		if ($rawFeed = $cache->get_data($feedURL, $feedURL)) {
-			$eventsDecoded = json_decode($rawFeed, TRUE);
-		} else {
-			$rawFeed = $cache->do_curl($feedURL);
-			$cache->set_cache($feedURL, $rawFeed);
-			$eventsDecoded = json_decode($rawFeed, TRUE);
-		}
+        $eventsDecoded = array();
+        //Temporary shim to allow for old method of fetching json
+        if(class_exists('quamsta\ApiCacher\FeedHelper')){
 
-		if (!empty($eventsDecoded)) {
-			return $eventsDecoded;
-		} else {
-			return false;
-		}
-	}
+            $eventsDecoded = FeedHelper::getJson($feedURL);
+
+        }else{
+
+            $cache = new SimpleCache();
+            if ($rawFeed = $cache->get_data($feedURL, $feedURL)) {
+                $eventsDecoded = json_decode($rawFeed, TRUE);
+            } else {
+                $rawFeed = $cache->do_curl($feedURL);
+                $cache->set_cache($feedURL, $rawFeed);
+                $eventsDecoded = json_decode($rawFeed, TRUE);
+            }
+
+        }
+
+        if (!empty($eventsDecoded)) {
+
+            return $eventsDecoded;
+        } else {
+
+            return false;
+        }
+    }
+
 }
